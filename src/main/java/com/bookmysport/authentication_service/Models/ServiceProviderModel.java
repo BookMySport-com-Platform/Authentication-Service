33bf4b1b1517c6f98b6338afece9f843b02da5cc
VortexProjects <vortex1649@gmail.com>
2024-01-25 14:59:40 +0530

    Add the centre name field in service provier model

--- src/main/java/com/bookmysport/authentication_service/Models/ServiceProviderModel.java
+++ src/main/java/com/bookmysport/authentication_service/Models/ServiceProviderModel.java
@@ -46,44 +46,7 @@ public class ServiceProviderModel {
     @NotNull
     private String address;
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    @NotNull
+    private String centreName;
 
 }
