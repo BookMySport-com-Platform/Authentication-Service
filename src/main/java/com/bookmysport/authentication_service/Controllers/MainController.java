7eed6bede5957d23977655ce110523540937c8d5
Anandateertha.dev <vortex1649@gmail.com>
2024-05-21 17:52:39 +0530

    Add cors config file

--- src/main/java/com/bookmysport/authentication_service/Controllers/MainController.java
+++ src/main/java/com/bookmysport/authentication_service/Controllers/MainController.java
@@ -8,7 +8,6 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -38,7 +37,6 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api")
-@CrossOrigin(origins="http://localhost:5173")
 public class MainController {
 
     @Autowired
